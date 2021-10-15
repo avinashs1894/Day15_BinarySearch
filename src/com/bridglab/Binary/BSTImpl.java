e15cb3914ff8a04ed26add2d82adb2dd7bb6ebe8
unknown <avinash.sankajji94@gmai.com>
2021-10-15 19:12:06 +0530

    Added:Insert Elements

---
+++ src/com/bridglab/Binary/BSTImpl.java
@@ -0,0 +1,16 @@
+package com.bridglab.Binary;
+
+public class BSTImpl {
+
+	public static void main(String[] args) {
+		BST tree = new BST();
+		Node root = null;
+		root = tree.insert(root,56);
+		root = tree.insert(root,30);
+		root = tree.insert(root,70);
+		
+		tree.printInorder(root);
+		System.out.println(root);
+		}
+
+}
